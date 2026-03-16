4e71b92c30f6a38b6a994b84d1ad5624f494da9b
wh0am1 <sd2770724@gmail.com>
2026-03-16 17:42:46 +0500

    Datamock removed

--- src/data.ts
+++ src/data.ts
@@ -6,66 +6,6 @@ export const dataMock = [
   { title: 'Ready', issues: [] },
   { title: 'In Progress', issues: [] },
   { title: 'Finished', issues: [] },
-  // {
-  //   title: 'Backlog',
-  //   issues: [
-  //     {
-  //       id: '1',
-  //       name: 'Sprint bugfix',
-  //       description: '‘Fix all the bugs’'
-  //     },
-  //     {
-  //       id: '2',
-  //       name: 'Login page – performance issues',
-  //       description: 'sldfjklj1k23m12'
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: 'Ready',
-  //   issues: [
-  //     {
-  //       id: '11',
-  //       name: 'Shop page – performance issues',
-  //       description: '‘Fix all the bugs’'
-  //     },
-  //     {
-  //       id: '12',
-  //       name: 'Checkout bugfix',
-  //       description: '‘Fix all the bugs’'
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: 'In Progress',
-  //   issues: [
-  //     {
-  //       id: '21',
-  //       name: 'User page – performance issues',
-  //       description: '‘Fix all the bugs’'
-  //     },
-  //     {
-  //       id: '22',
-  //       name: 'Auth bugfix',
-  //       description: '‘Fix all the bugs’'
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: 'Finished',
-  //   issues: [
-  //     {
-  //       id: '31',
-  //       name: 'Main page – performance issues',
-  //       description: '‘Fix all the bugs’'
-  //     },
-  //     {
-  //       id: '32',
-  //       name: 'Main page bugfix',
-  //       description: '‘Fix all the bugs’'
-  //     }
-  //   ]
-  // },
 ]
 
 export const SELECT_STYLE: StylesConfig<TaskOption, false> = {
